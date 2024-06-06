d0321ddb1f814f251bf7c16c24d2e2f457e2041d
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-06-06 08:10:11 -0600

    updated CSS, started troubleshooting JS file

--- assets/JS/form.js
+++ assets/JS/form.js
@@ -1,14 +1,20 @@
+const usernameInput = document.querySelector('#username');
+const titleInput = document.querySelector('#title');
+const contentInput = document.querySelector('#content');
+
 function handleFormsubmit(event) {
     event.preventDefault();
 
     const formData = {
-        username: document.getElementById('username').value,
-        title: document.getElementById('title').value,
-        content: document.getElementById('content').value
+        username: document.localStorage.getElementById('username').value,
+        title: document.localStorage.getElementById('title').value,
+        content: document.localStorage.getElementById('content').value
 
     };
 
 localStorage.setItem('formData', JSON.stringify(formData));
-
 console.log(formData);
+window.location.href = 'blog.html';
+
 }
+
